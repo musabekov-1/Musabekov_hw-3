7e9d7601a861e5aa9907f3ff93938425e87fa11f
musabekov-1 <bestmusabekov@gmail.com>
2023-10-16 17:23:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(15000);
+        try {
+            while(true){
+                bankAccount.withDraw(6000);
+                System.out.println("Снято 6000 сом. Остаток: " + bankAccount.getAmount());
+            }
+        }catch (LimitException e){
+            try {
+                bankAccount.withDraw((int) e.getRemainingAmount());
+            }catch (LimitException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        System.out.println(bankAccount.getAmount());
+    }
+}
